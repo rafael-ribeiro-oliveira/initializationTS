bc958afd101882cbf9c1987e176e4c0a1ee8ed71
Rafael Oliveira <rafaeloliver789@gmail.com>
2025-07-25 16:25:53 -0300

    Express com TS

--- 13_express_com_ts/src/App.ts
+++ 13_express_com_ts/src/App.ts
@@ -2,12 +2,21 @@
 //console.log("Express + TS");
 
 // 2 - init express
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
 // 3 - rota com POST
 app.use(express.json());
+
+// 11 - middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+    console.log(req.path);
+    next();
+}
+
+app.use(showPath);
+
 
 app.get("/", (req, res) => {
     return res.send("Hello Express!");
@@ -67,9 +76,67 @@ app.get("/api/product/:id", (req: Request, res: Response) => {
     } else {
         return res.send("Produto não encontrado!");
     }
+});
 
+// 8 - rotas complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+    console.log(req.params);
+
+    const productId = req.params.id;
+    const reviewId = req.params.reviewId;
+
+    return res.send(`Acessando o review ${reviewId} do produto ${productId}`);
     
 });
+
+// 9  - router handler
+function getUser(req: Request, res: Response) {
+    console.log(`Resgatando o usuário com id: ${req.params.id}`);
+
+    return res.send("O usuário foi encontrado");
+}
+
+app.get("/api/user/:id", getUser);
+
+// 10 - middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+    if(req.params.id === "1") {
+        console.log("Pode seguir!");
+        next();
+    }   else  {
+        console.log("Acesso restrito");
+        
+    }
+}
+
+app.get("/api/user/:id/acecess", checkUser, (req: Request, res: Response) => {
+    return res.json({ msg: "Bem-vindo a área administrável" });
+});
+
+// 12 - req e res com generics
+app.get(
+    "/api/user/:id/details/:name",
+    (
+        req: Request<{id: string; name: string }>,
+        res: Response<{ status: boolean }>
+    ) => {
+        console.log(`ID: ${req.params.id}`);
+        console.log(`Name: ${req.params.name}`);
+
+        return res.json({status: true});
+    }
+);
+
+// 13 - tratando erros
+app.get("/api/error", (req: Request, res: Response) => {
+    try {
+        // a nossa lógicaa
+        throw new Error("Algo deu errado!");
+    } catch (e: any) {
+        res.status(500).json({ msg: e.message });
+    }
+});
+
 
 app.listen(3000, () => {
     console.log("Aplicação de TS + Express funcionando!");
